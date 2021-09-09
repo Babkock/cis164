ed07aed7c53276721690c55bbed4cce5850ef7dc
Tanner Babcock <babkock@protonmail.com>
2021-09-09 03:37:28 -0500

    Working on food track + GitLab CI

--- foodtruck.cpp
+++ foodtruck.cpp
@@ -1,5 +1,6 @@
 /* Food truck program
  * Tanner Babcock
+ * tababcock@dmacc.edu
  * September 8, 2021
 */
 #include <iostream>
@@ -26,6 +27,8 @@ int soda = MAXSODA;
 extern double cashRegister;
 double cashRegister = 0.0;
 
+/* Sells hamburgers or chili hamburgers, returns the total amount
+ * of the transaction */
 double sellHamburger(bool chili, int quantity = 1) {
 	double total; // total amount due from customer
 	double tax;   // added sales tax
@@ -45,9 +48,11 @@ double sellHamburger(bool chili, int quantity = 1) {
 	return (total + tax);
 }
 
+/* Sells hotdogs or chili hotdogs, returns the total amount
+ * of the transaction */
 double sellHotdog(bool chili, int quantity = 1) {
-	double total; // total amount due
-	double tax;   // added sales tax
+	double total;
+	double tax;
 
 	if (chili) {
 		total = (7.00 * quantity);
@@ -96,6 +101,16 @@ void showMenu(void) {
 	return;
 }
 
+void inventory(void) {
+	int x, y, z;
+	cout << "Enter the amount of hamburger Buns available (currently " << burgerBuns << "): ";
+	cin >> x;
+	burgerBuns = x;
+	cout << "Enter the amount of hamburger Patties available (currently " << burgerPatties << "): ";
+	cin >> y;
+	burgerPatties = y;
+}
+
 int main(void) {
 	char menuOption;
 	int quantity;
@@ -106,8 +121,43 @@ int main(void) {
 	double dog = sellHotdog(false, 2);
 
 	cout.precision(2);
+	/*
 	cout << "3 chili burgers total: $" << fixed << burger << endl;
 	cout << "2 hotdogs total: $" << fixed << dog << endl;
+	*/
+	cout << "Cash register: $" << cashRegister << endl;
+
+	showMenu();
+	do {
+		if (burgerBuns <= 15) {
+			cout << "Running low on hamburger buns, only " << burgerBuns << " left" << endl;
+		}
+		if (hotdogBuns <= 15) {
+			cout << "Running low on hotdog buns, only " << hotdogBuns << " left" << endl;
+		}
+		if (burgerPatties < 10) {
+			cout << "Running low on hamburger patties, only " << burgerPatties << " left" << endl;
+		}
+		if (hotdogs < 10) {
+			cout << "Running low on hotdogs, only " << hotdogs << " left" << endl;
+		}
+		if (chili <= 100) {
+			cout << "Running low on chili, only " << chili << " ounces left" << endl;
+		}
+		if (fryBaskets <= 15) {
+			cout << "Running low on fry baskets, only " << fryBaskets << " left" << endl;
+		}
+		if (soda <= 40) {
+			cout << "Running low on soda, only " << soda << " cans left" << endl;
+		}
+		cout << "Action: ";
+		cin >> menuOption;
+		cout << "menuOption == '" << menuOption << "'" << endl;
+
+		if (menuOption == 'i') {
+			inventory();
+		}
+	} while (menuOption != 'q');
 
 	return 0;
 }
