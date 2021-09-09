a388955ed63e1b87b0658cf7ad44be39a2520c1b
Tanner Babcock <babkock@protonmail.com>
2021-09-08 19:08:52 -0500

    Food truck

--- foodtruck.cpp
+++ foodtruck.cpp
@@ -1,7 +1,50 @@
+/* Food truck program
+ * Tanner Babcock
+ * September 8, 2021
+*/
 #include <iostream>
+#define MAXBUNS 75
+#define MAXSODA 200
+#define MAXBURGERS 200
+#define MAXCHILI 500
 using namespace std;
 
+extern int burgerPatties;
+extern int burgerBuns;
+extern int hotdogs;
+extern int hotdogBuns;
+extern int chili;
+extern int fryBaskets;
+extern int soda;
+extern double cashRegister;
+
+double sellHamburger(bool chili, int quantity = 1) {
+	
+}
+
+double sellHotdog(bool chili, int quantity = 1) {
+	
+}
+
+void showMenu(void) {
+	
+}
+
 int main(void) {
+	burgerPatties = MAXBURGERS;
+	burgerBuns = MAXBUNS;
+	hotdogs = MAXBURGERS;
+	hotdogBuns = MAXBUNS;
+	chili = MAXCHILI;
+	fryBaskets = MAXBURGERS;
+	soda = MAXSODA;
+	cashRegister = 0.0;
+	
+	char menuOption;
+	int quantity;
+	double subTotal;
+	double tax;
+	
 	return 0;
 }
 
