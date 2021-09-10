309d40176e3df51de6545c02bf5126c4798810af
Tanner Babcock <babkock@protonmail.com>
2021-09-09 21:53:34 -0500

    Food truck progress

--- foodtruck.cpp
+++ foodtruck.cpp
@@ -84,6 +84,7 @@ void showMenu(void) {
 	cout << "'q' - Quits the program" << endl;
 	cout << "'h' - Shows this menu" << endl;
 	cout << "'m' - Shows this menu" << endl;
+    cout << "'r' - Show cash register balance" << endl;
 	
 	if ((burgerBuns > 0) && (burgerPatties > 0)) {
 		cout << "'b' - Hamburger(s) $5.00 - " << burgerBuns << " left" << endl;
@@ -112,56 +113,150 @@ void showMenu(void) {
 /* Allows the user to input the inventory, showing current values
  * and warning for values that are too high */
 void inventory(void) {
-	int x, y, z;
+	int x, y, z, a;
 	cout << "Enter the amount of hamburger Buns available (currently " << burgerBuns << "): ";
 	cin >> x;
-	burgerBuns = x;
+	if (x > MAXBUNS) {
+		cerr << "We don't have enough room for that many, input ignored" << endl;
+	}
+	else {
+		burgerBuns = x;
+	}
 	cout << "Enter the amount of hamburger Patties available (currently " << burgerPatties << "): ";
 	cin >> y;
-	burgerPatties = y;
+	if (y > MAXBURGERS) {
+		cerr << "We don't have enough room for that many, input ignored" << endl;
+	}
+	else {
+		burgerPatties = y;
+	}
+	cout << "Enter the amount of hotdog Buns available (currently " << hotdogBuns << "): ";
+	cin >> z;
+	if (z > MAXBUNS) {
+		cerr << "We don't have enough room for that many, input ignored" << endl;
+	}
+	else {
+		hotdogBuns = z;
+	}
+	x = 0;
+	cout << "Enter the amount of hotdogs available (currently " << hotdogs << "): ";
+	cin >> x;
+	if (x > MAXBURGERS) {
+		cerr << "We don't have enough room for that many, input ignored" << endl;
+	}
+	else {
+		hotdogs = x;
+	}
+	y = 0;
+	cout << "Enter the amount of chili available (currently " << chili << " ounces): ";
+	cin >> y;
+	if (y > MAXCHILI) {
+		cerr << "We don't have enough room for that much, input ignored" << endl;
+	}
+	else {
+		chili = y;
+	}
+	z = 0;
+	cout << "Enter the amount of fry baskets available (currently " << fryBaskets << " baskets): ";
+	cin >> z;
+	if (z > MAXBUNS) {
+		cerr << "We don't have enough room for that many, input ignored" << endl;
+	}
+	else {
+		fryBaskets = z;
+	}
+	cout << "Enter the amount of soda available (currently " << soda << " cans): ";
+	cin >> a;
+	if (a > MAXSODA) {
+		cerr << "We don't have enough room for that much, input ignored" << endl;
+	}
+	else {
+		soda = a;
+	}
+	cout << "Finished inputting inventory" << endl;
 }
 
 int main(void) {
 	char menuOption;
 	int quantity;
-	double subTotal;
-	double tax;
-	
-	double burger = sellHamburger(true, 3);
-	double dog = sellHotdog(false, 2);
+    double subTotal, tax;
+    int soldBurgers = 0, soldChiliBurgers = 0;
+    int soldHotdogs = 0, soldChiliHotdogs = 0;
+    int soldChili = 0, soldFryBaskets = 0, soldSoda = 0;
 
 	cout.precision(2);
 	cout << "Cash register: $" << cashRegister << endl;
 
 	showMenu();
 	do {
-		if (burgerBuns <= 15) {
+		if (burgerBuns <= LOWBUNS) {
 			cout << "Running low on hamburger buns, only " << burgerBuns << " left" << endl;
 		}
-		if (hotdogBuns <= 15) {
+		if (hotdogBuns <= LOWBUNS) {
 			cout << "Running low on hotdog buns, only " << hotdogBuns << " left" << endl;
 		}
-		if (burgerPatties < 10) {
+		if (burgerPatties < LOWBURGERS) {
 			cout << "Running low on hamburger patties, only " << burgerPatties << " left" << endl;
 		}
-		if (hotdogs < 10) {
+		if (hotdogs < LOWBURGERS) {
 			cout << "Running low on hotdogs, only " << hotdogs << " left" << endl;
 		}
-		if (chili <= 100) {
+		if (chili <= LOWCHILI) {
 			cout << "Running low on chili, only " << chili << " ounces left" << endl;
 		}
-		if (fryBaskets <= 15) {
+		if (fryBaskets <= LOWBUNS) {
 			cout << "Running low on fry baskets, only " << fryBaskets << " left" << endl;
 		}
-		if (soda <= 40) {
+		if (soda <= LOWSODA) {
 			cout << "Running low on soda, only " << soda << " cans left" << endl;
 		}
 		cout << "Action: ";
 		cin >> menuOption;
-		cout << "menuOption == '" << menuOption << "'" << endl;
 
-		if (menuOption == 'i') {
-			inventory();
+		switch (menuOption) {
+			case 'i': case 'I':
+				inventory();
+				break;
+            case 'b': case 'B': // hamburgers
+                if ((burgerBuns == 0) || (burgerPatties == 0))
+                    cerr << "Sorry no more burgers, because there are no more buns" << endl;
+                else {
+                    cout << "How many hamburgers? ";
+                    cin >> quantity;
+                    if ((burgerBuns - quantity) >= 0) {
+                        subTotal = (double)sellHamburger(false, quantity);
+                        soldBurgers += quantity;
+                        // sellHamburger() takes care of tax
+                        cout << "$" << fixed << subTotal << endl;
+                    }
+                    else {
+                        cerr << "We don't have that many hamburgers" << endl;
+                    }
+                }
+                break;
+            case 'e': case 'E': // chili burgers
+                if (burgerBuns == 0 || burgerPatties == 0)
+                    cerr << "Sorry no more burgers, because there are no more buns" << endl;
+                else if (chili < 4)
+                    cerr << "Sorry not enough chili" << endl;
+                else {
+                    cout << "How many chili burgers? ";
+                    cin >> quantity;
+                    if ((burgerBuns - quantity) >= 0) {
+                        subTotal = (double)sellHamburger(true, quantity);
+                        cout << "$" << fixed << subTotal << endl;
+                    }
+                    else {
+                        cerr << "We don't have that many hamburgers" << endl;
+                    }
+                }
+                break;
+			case 'h': case 'H': case 'm': case 'M':
+				showMenu();
+				break;
+            default:
+                cerr << "Unknown menu option specified" << endl;
+                break;
 		}
 	} while (menuOption != 'q');
 
