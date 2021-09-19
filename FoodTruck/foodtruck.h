89e0a99e5997d5c9908809432b1597f3cc01a145
Tanner Babcock <babkock@protonmail.com>
2021-09-19 05:51:57 -0500

    I AM ALMOST DONE

--- FoodTruck/foodtruck.h
+++ FoodTruck/foodtruck.h
@@ -6,6 +6,7 @@
 #ifndef FOODTRUCK_H
 #define FOODTRUCK_H
 
+#include <vector>
 #include <string>
 
 #define MAXBUNS 75
@@ -19,6 +20,11 @@
 #define ENTREE 5.0
 #define CENTREE 7.0
 
+typedef struct {
+    int quantity;
+    double total;
+} transaction;
+
 extern int burgerPatties;
 extern int burgerBuns;
 extern int hotdogs;
@@ -28,16 +34,25 @@ extern int fryBaskets;
 extern int soda;
 extern double cashRegister;
 
-typedef struct {
-    int quantity;
-    double total;
-} transaction;
+extern std::vector<transaction> soldBurgers;
+extern std::vector<transaction> soldHotdogs;
+extern std::vector<transaction> chiliBurgers;
+extern std::vector<transaction> chiliHotdogs;
+extern std::vector<transaction> soldChili;
+extern std::vector<transaction> soldBaskets;
+extern std::vector<transaction> soldSoda;
 
+/* purchase.cpp */
 double sellHamburger(bool ch, int quantity);
 double sellHotdog(bool ch, int quantity);
+
+/* menu.cpp */
 int iprompt(const std::string & p);
 char cprompt(const std::string & p);
 void showMenu(void);
 void inventory(void);
+
+/* sales.cpp */
+void salesReport(std::string filename);
 
 #endif
--- FoodTruck/main.cpp
+++ FoodTruck/main.cpp
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <fstream>
 #include "foodtruck.h"
 using namespace std;
 
@@ -19,18 +18,18 @@ int fryBaskets = MAXBUNS;
 int soda = MAXSODA;
 double cashRegister = 0.0;
 
+vector<transaction> soldBurgers;
+vector<transaction> soldHotdogs;
+vector<transaction> chiliBurgers;
+vector<transaction> chiliHotdogs;
+vector<transaction> soldChili;
+vector<transaction> soldBaskets;
+vector<transaction> soldSoda;
+
 int main(void) {
     char menuOption;
     int quantity;
     double subTotal, tax;
-
-    vector<transaction> soldBurgers;
-    vector<transaction> soldHotdogs;
-    vector<transaction> chiliBurgers;
-    vector<transaction> chiliHotdogs;
-    vector<transaction> soldChili;
-    vector<transaction> soldBaskets;
-    vector<transaction> soldSoda;
 
     cout.precision(2);
     cout << "Cash register: $" << fixed << cashRegister << endl;
@@ -211,122 +210,7 @@ int main(void) {
     cout << "Type a filename, then ENTER, to write the sales report" << endl << "or press CTRL + D to print it:" << endl;
     cin >> filename;
 
-    if (!filename.empty()) {
-        ofstream salesReport;
-        salesReport.open(filename.c_str());
-        salesReport.precision(2);
-        cout << "Written sales report to " << filename << endl;
-
-        salesReport << "Hamburgers sold: " << soldBurgers.size() << endl;
-        salesReport << "Hamburger sales:" << endl;
-        salesReport << "-----------------" << endl;
-
-        double z = 0.0;
-        for (const transaction& t : soldBurgers) {
-            salesReport << t.quantity << "\t\t$" << fixed << t.total << endl;
-            z += t.total;
-        }
-        salesReport << "-----------------" << endl;
-        salesReport << "Total made from hamburgers: $" << fixed << z << endl;
-        salesReport << "-----------------" << endl;
-
-        salesReport << "Hotdogs sold: " << soldHotdogs.size() << endl;
-        salesReport << "Hotdog sales:" << endl;
-        salesReport << "-----------------" << endl;
-        z = 0.0;
-        for (const transaction& t : soldHotdogs) {
-            salesReport << t.quantity << "\t\t$" << fixed << t.total << endl;
-            z += t.total;
-        }
-        salesReport << "-----------------" << endl;
-        salesReport << "Total made from hotdogs: $" << fixed << z << endl;
-        salesReport << "-----------------" << endl;
-
-        salesReport << "Chili burgers sold: " << chiliBurgers.size() << endl;
-        salesReport << "Chili burger sales:" << endl;
-        salesReport << "-----------------" << endl;
-        z = 0.0;
-        for (const transaction& t : chiliBurgers) {
-            salesReport << t.quantity << "\t\t$" << fixed << t.total << endl;
-            z += t.total;
-        }
-        salesReport << "-----------------" << endl;
-        salesReport << "Total made from chili burgers: $" << fixed << z << endl;
-        salesReport << "-----------------" << endl;
-
-        salesReport << "Chili hotdogs sold: " << chiliHotdogs.size() << endl;
-        salesReport << "Chili hotdog sales:" << endl;
-        salesReport << "-----------------" << endl;
-        z = 0.0;
-        for (const transaction& t : chiliHotdogs) {
-            cout << t.quantity << "\t\t$" << fixed << t.total << endl;
-            z += t.total;
-        }
-        salesReport << "-----------------" << endl;
-        salesReport << "Total made from chili hotdogs: $" << fixed << z << endl;
-        salesReport << "-----------------" << endl;
-
-        salesReport << "Fry baskets sold: " << soldBaskets.size() << endl;
-        salesReport << "Fry basket sales:" << endl;
-        salesReport << "-----------------" << endl;
-        z = 0.0;
-        for (const transaction& t : soldBaskets) {
-            cout << t.quantity << "\t\t$" << fixed << t.total << endl;
-            z += t.total;
-        }
-        salesReport << "-----------------" << endl;
-        salesReport << "Total made from fry baskets: $" << fixed << z << endl;
-        salesReport << "-----------------" << endl;
-        salesReport << "Total money: $" << fixed << cashRegister << endl;
-
-        salesReport.close();
-    }
-    else {
-        cout << "Hamburgers sold: " << soldBurgers.size() << endl;
-        cout << "Hamburger sales:" << endl;
-        double z = 0.0;
-        for (const transaction& t : soldBurgers) {
-            cout << t.quantity << "\t\t$" << fixed << t.total << endl;
-            z += t.total;
-        }
-        cout << "Total made from hamburgers: $" << fixed << z << endl << endl;
-
-        cout << "Hotdogs sold: " << soldHotdogs.size() << endl;
-        cout << "Hotdog sales:" << endl;
-        z = 0.0;
-        for (const transaction& t : soldHotdogs) {
-            cout << t.quantity << "\t\t$" << fixed << t.total << endl;
-            z += t.total;
-        }
-        cout << "Total made from hotdogs: $" << fixed << z << endl << endl;
-
-        cout << "Chili burgers sold: " << chiliBurgers.size() << endl;
-        cout << "Chili burger sales:" << endl;
-        z = 0.0;
-        for (const transaction& t : chiliBurgers) {
-            cout << t.quantity << "\t\t$" << fixed << t.total << endl;
-            z += t.total;
-        }
-        cout << "Total made from chili burgers: $" << fixed << z << endl << endl;
-
-        cout << "Chili hotdogs sold: " << chiliHotdogs.size() << endl;
-        cout << "Chili hotdog sales:" << endl;
-        z = 0.0;
-        for (const transaction& t : chiliHotdogs) {
-            cout << t.quantity << "\t\t$" << fixed << t.total << endl;
-            z += t.total;
-        }
-        cout << "Total made from chili hotdogs: $" << fixed << z << endl << endl;
-
-        cout << "Fry baskets sold: " << soldBaskets.size() << endl;
-        cout << "Fry basket sales:" << endl;
-        z = 0.0;
-        for (const transaction& t : soldBaskets) {
-            cout << t.quantity << "\t\t$" << fixed << t.total << endl;
-            z += t.total;
-        }
-        cout << "Total made from fry baskets: $" << fixed << z << endl << endl;
-    }
+    salesReport(filename);
 
     return 0;
 }
