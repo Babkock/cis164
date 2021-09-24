6b352d5a641cf72005d1a82a63278a513dad8399
Tanner Babcock <babkock@protonmail.com>
2021-09-24 02:55:48 -0500

    Finished CSV writer

--- csvwrite.cpp
+++ csvwrite.cpp
@@ -58,26 +58,46 @@ int main(void) {
 	Order iterate;
 	string filename;
 	ofstream orders;
+	int y = 0;
 	
 	cout << "Please enter a filename to write orders to: ";
 	cin >> filename;
 	orders.open(filename, ios::out);
-
-	cout.precision(2);
+	orders.precision(2);
+	
 	do {
-		cout << "Doing it again" << endl;
+		y++;
+		cout << "Entering order #" << y << endl;
 		if (!iterate.getSku().empty()) {
-//			cout << "You entered: " << o.getSku() << "," << o.getQuantity() << "," << fixed << o.getPrice() << endl;
 			list.push_back(iterate);
 		}
 	} while (iterate.promptValues());
 	/* This will break the loop if the user enters a 'q' */
 
-	int y = 1;
+	y = 1;
 	for (Order x : list) {
 		cout << "Writing order #" << y << endl;
-		
+		orders << x.getSku() << "," << x.getQuantity() << "," << fixed << x.getPrice() << endl;
 		y++;
 	}
+
+	orders << endl;
+	orders.close();
 	return 0;
 }
+
+/* 
+Please enter a filename to write orders to: test.csv
+Entering order #1
+SKU (q to quit): r1hf72
+Quantity: 40
+Unit Price: 4.20
+Entering order #2
+SKU (q to quit): p00p3r
+Quantity: 666
+Unit Price: 4.69
+Entering order #3
+SKU (q to quit): q
+Writing order #1
+Writing order #2
+*/
