8516c81ca21be4aa2c91ad124f74ab6a134284df
Tanner Babcock <babkock@protonmail.com>
2021-10-02 16:06:41 -0500

    Biometrics assignment

---
+++ biometrics.cpp
@@ -0,0 +1,17 @@
+/* Biometrics addition
+ * Tanner Babcock
+ * tababcock@dmacc.edu
+ * October 2, 2021 */
+#include <iostream>
+#include <vector>
+#include <memory>
+using namespace std;
+
+class Biometrics {
+	
+};
+
+int main(void) {
+
+	return 0;
+}
