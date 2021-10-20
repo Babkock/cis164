fe0b844f2ce18dfbc3bb4a10295b73250988df8f
Tanner Babcock <babkock@protonmail.com>
2021-10-20 15:28:37 -0500

    User Profile assignment

---
+++ account.cpp
@@ -0,0 +1,12 @@
+/* User Profile
+ * Tanner Babcock
+ * tababcock@dmacc.edu
+ * October 20, 2021 */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+
+	return 0;
+}
