0719bbe204ed14078120675ccd1d09270c5694af
Tanner Babcock <babkock@protonmail.com>
2021-10-23 16:05:15 -0500

    Operators done

--- operators.cpp
+++ operators.cpp
@@ -37,6 +37,7 @@ public:
 	}
 };
 
+/* addition */
 double operator+(Shape &s1, Shape &s2) {
 	return s1.area() + s2.area();
 }
@@ -45,15 +46,56 @@ double operator+(double area, Shape &s) {
 	return area + s.area();
 }
 
+/* subtraction */
+double operator-(Shape &s1, Shape &s2) {
+	return s1.area() - s2.area();
+}
+
+double operator-(double area, Shape &s) {
+	return area - s.area();
+}
+
+/* multiplication */
+double operator*(Shape &s1, Shape &s2) {
+	return s1.area() * s2.area();
+}
+
+double operator*(double area, Shape &s) {
+	return area * s.area();
+}
+
 int main(void) {
 	Rectangle shape1(10, 10);
 	Rectangle shape2(5, 10);
 	Circle shape3(5);
+	cout << "Shape1 area: " << shape1.area() << endl;
+	cout << "Shape2 area: " << shape2.area() << endl;
+	cout << "Shape3 area: " << shape3.area() << endl;
 
 	// adding shape objects together returns the sum of their areas
 	double total = shape1 + shape2 + shape3;
 	cout << "The total area is " << total << endl;
 
+	cout << "Shape1 minus Shape2: " << (shape1 - shape2) << endl;
+	cout << "Shape3 minus Shape2: " << (shape3 - shape2) << endl;
+	cout << "Shape1 times Shape2: " << (shape1 * shape2) << endl;
+
 	return 0;
 }
+
+/* A program like this could be used by landscaping or construction
+ * companies, to find out how much grass, or metal, or concrete
+ * they need, to cover multiple buildings */
+
+/* Output:
+
+Shape1 area: 100
+Shape2 area: 50
+Shape3 area: 78.5
+The total area is 228.5
+Shape1 minus Shape2: 50
+Shape3 minus Shape2: 28.5
+Shape1 times Shape2: 5000
+
+*/
 
