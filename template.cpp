1644e4ce94efda4e3ed4a573fe569850ee930ebf
Tanner Babcock <babkock@protonmail.com>
2021-10-24 18:53:17 -0500

    Template function with objects

--- template.cpp
+++ template.cpp
@@ -8,14 +8,25 @@ using namespace std;
 
 int main(void) {
 	int a = 5, b = 6, c = 7;
+	double d = 10.25, e = 20.4, f = 30.7;
+	Obj o(500, "hello world");
+	Obj p(600, "goodbye");
+
 	cout << "The bigger number of " << c << " and " << a << " is " << max<int>(c, a) << endl;
 
-	double d = 10.25, e = 20.4, f = 30.7;
 	cout.precision(2);
 	cout << "The bigger number of " << fixed << d;
-	cout << " and " << fixed << f << " is " << fixed << max<double>(d, f) << endl;
+	cout << " and " << f << " is " << max<double>(d, f) << endl;
 
 	cout << "The square of 5 is " << square<int>(5) << endl;
+
+	cout << "The bigger object between this: " << endl;
+	o.print();
+	cout << "And this:" << endl;
+	p.print();
+	cout << "Is this:" << endl;
+	/* should print 600.00 "goodbye" */
+	max<Obj>(o, p).print();
 
 	return 0;
 }
@@ -25,4 +36,11 @@ int main(void) {
 The bigger number of 7 and 5 is 7
 The bigger number of 10.25 and 30.70 is 30.70
 The square of 5 is 25
+The bigger object between this:
+This object is: 500.00 "hello world"
+And this:
+This object is: 600.00 "goodbye"
+Is this:
+This object is: 600.00 "goodbye"
+
 */
--- util.h
+++ util.h
@@ -5,6 +5,41 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+/* The template functions below will not work with this
+ * object type unless we overload operators */
+class Obj {
+private:
+	double value;
+	std::string text;
+public:
+	Obj(double v, const std::string &t): value(v), text(t) { }
+	~Obj(void) {
+		value = 0;
+		text = "";
+	}
+	double getValue(void) { return value; }
+	std::string getText(void) { return text; }
+	void print(void) {
+		std::cout << "This object is: " << value << " \"" << text << "\"" << std::endl;
+	}
+};
+
+bool operator>(Obj &o1, Obj &o2) {
+	return (o1.getValue() > o2.getValue());
+}
+
+bool operator>(double a, Obj &o) {
+	return (a > o.getValue());
+}
+
+double operator*(Obj &o1, Obj &o2) {
+	return (o1.getValue() * o2.getValue());
+}
+
+double operator*(double a, Obj &o) {
+	return (a * o.getValue());
+}
+
 template <class T>
 T &max(T &a, T &b) {
 	return ((a > b) ? a : b);
