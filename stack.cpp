f692d5a5441aa35a3eb522d387bbe177887b92a1
Tanner Babcock <babkock@protonmail.com>
2021-11-04 20:57:37 -0500

    Stack assignment

---
+++ stack.cpp
@@ -0,0 +1,15 @@
+/* Using the Stack
+ * Tanner Babcock
+ * tababcock@dmacc.edu
+ * November 4, 2021 */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const int NUM_POINTS = 5;
+const double SCALE = 0.5;
+
+class Point {
+	
+};
+
